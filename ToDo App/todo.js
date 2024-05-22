cff4ae2d3a293a8c20befeffebf886eaecc44031
Riya Nayan <riyanayan@Riyas-MacBook-Air.local>
2024-05-23 00:02:57 +0530

    Delete, Update functionality and all To do list CSS

--- ToDo App/todo.js
+++ ToDo App/todo.js
@@ -1,13 +1,23 @@
-let todoList = ["buy milk", "learn js"];
+let todoList = [
+  {
+    items: "buy milk",
+    dueDate: "22/05/2024",
+  },
+  {
+    items: "learn js",
+    dueDate: "22/05/2024",
+  },
+];
 displayItems();
 function addTodo() {
   let inputElement = document.querySelector("#todo-input");
-  console.log("inputElement: ", inputElement);
+  let dateElement = document.querySelector("#todo-date");
   let todoItem = inputElement.value;
-  console.log("todoItem: ", todoItem);
-  todoList.push(todoItem);
-  console.log("todoList: ", todoList);
+  let todoDate = dateElement.value;
+  //pushing an object here
+  todoList.push({ items: todoItem, dueDate: todoDate });
   inputElement.value = "";
+  dateElement.value = ""; //Input elemts value is set to blank
   displayItems();
 }
 
@@ -17,10 +27,15 @@ function displayItems() {
   let newHtml = "";
 
   for (let i = 0; i < todoList.length; i++) {
+    // let todoItem = todoList[i].item;
+    // let todoDate = todoList[i].dueDate;
+    let { items, dueDate } = todoList[i];
     newHtml += `
     <div>
-    <span>${todoList[i]}</span>
-    <button onclick="todoList.splice(${i}, 1); displayItems();">Delete</button> 
+    <span class="items-text">${items}</span>
+    <span class="date-text">${dueDate}</span>
+    <button class="delete-btn" onclick="todoList.splice(${i}, 1); displayItems();">Delete</button> 
+    <button class="update-btn" onclick="updateTodo();">Update</button>
     </div>
     `;
   }
